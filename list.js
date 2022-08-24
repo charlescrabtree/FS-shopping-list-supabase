35f45d9237c2046eb0dd175337fe5ef5af8a9a5f
charlescrabtree <charles.crabtree.horn@gmail.com>
2022-08-24 12:15:03 -0700

    added eventlistener to form

--- list.js
+++ list.js
@@ -2,7 +2,7 @@
 // working with supabase:
 import { checkAuth, signOutUser } from './fetch-utils.js';
 // pure rendering (data --> DOM):
-
+import { renderItmes } from './render-utils.js';
 /*  "boiler plate" auth code */
 // checking if we have a user! (will redirect to auth if not):
 checkAuth();
@@ -15,9 +15,25 @@ signOutLink.addEventListener('click', signOutUser);
 /* end "boiler plate auth code" */
 
 // grab needed DOM elements on page:
+const listContainer = document.getElementById('list-container');
+const form = document.querySelector('form');
+const deleteButton = document.getElementById('delete-button');
 
 // local state:
+let itemsArr= [];
 
-// display functions:
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
 
+    const data = new FormData(form);
+    const item = data.get('item');
+    const quantity = data.get('quantity');
+
+    const newItem = await createItem(item, quantity);
+    itemsArr.push(newItem);
+
+    await displayItems();
+
+    form.reset();
+});
 // events:
